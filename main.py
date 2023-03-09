20849b9fc45d77b204383ba4a1ea082830845692
Mubin17 <48038715+Mubin17@users.noreply.github.com>
2023-03-08 23:22:51 -0700

    main.py removed

--- main.py
+++
@@ -1,5 +0,0 @@
-from Tracklist import tracklist1001_url_to_youtube_comment
-
-Chrome_driver_path = '/Users/mubinmanasia/Downloads/chromedriver'
-Url = 'https://1001.tl/rn1qu9t'
-tracklist1001_url_to_youtube_comment(Chrome_driver_path, Url)
